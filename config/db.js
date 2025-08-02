3e39dfbdc0e414f096240ba3361d450a20c2a9b4
spectatorfree <spectator@email.com>
2025-08-02 15:12:43 +0500

    Updated db.js for local and production compatibility

--- config/db.js
+++ config/db.js
@@ -1,13 +1,27 @@
 const { Pool } = require('pg');
+require('dotenv').config();
 
-const pool = new Pool({
-  user: 'postgres',
-  host: 'localhost',
-  database: 'choizze_db',
-  password: '2378',
-  port: 5432,
-});
+let pool;
+
+if (process.env.DATABASE_URL) {
+    // Если переменная окружения DATABASE_URL существует (как на Render), используем ее
+    pool = new Pool({
+        connectionString: process.env.DATABASE_URL,
+        ssl: {
+            rejectUnauthorized: false
+        }
+    });
+} else {
+    // В противном случае, используем локальные настройки
+    pool = new Pool({
+        user: 'postgres',
+        host: 'localhost',
+        database: 'choizze_db',
+        password: '2378',
+        port: 5432,
+    });
+}
 
 module.exports = {
-  query: (text, params) => pool.query(text, params),
+    query: (text, params) => pool.query(text, params),
 };
